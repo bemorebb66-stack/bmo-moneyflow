d4e4b7d389b2295801215155cd37765b4bc8efe7
ssoni <bemorebb66@gmail.com>
2026-08-09 16:01:18 +0900

    deploy: publish favorites and right rail

---
+++ assets/stocks._ticker-Cp3mVv7W.js
@@ -0,0 +1,1 @@
+import{Yt as e}from"./page-shell-BSVpEkMg.js";import{i as t,n,t as r}from"./stocks._ticker-CSDFDXBO.js";var i=e();function a(){let{ticker:e}=r.useParams();return(0,i.jsx)(t,{ticker:n(e)})}export{a as component};
